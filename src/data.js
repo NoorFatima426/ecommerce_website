aa1e978321ca02ac483a6fa0492e7a14117ca8cf
Noor Fatima <nf65027@gmail.com>
2024-07-24 01:21:59 -0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,126 @@
+let shopItemsData=[
+  { id:"ghi",
+    name:"Boys Shirts",
+    price :35,
+    desc:"Brown Color Beautiful Shirt",
+    img:"/imgs/d2.jpg"},
+    { id:"lmn",
+      name:"Boys Shirts",
+      price :40,
+      desc:"Collection For Boys",
+      img:"/imgs/b1.jpg"},
+      { id:"opq",
+        name:"Boys Shirts",
+        price :55,
+        desc:"Boys Collection",
+        img:"/imgs/b2.jpg"},
+        { id:"rst",
+          name:"Boys Shirts",
+          price :35,
+          desc:"Comfort High Necks For Winter",
+          img:"/imgs/b3.jpg"},
+          { id:"uvw",
+            name:"Girls Shirts",
+            price :55,
+            desc:"Light tea pink women kurta",
+            img:"/imgs/g1.png"},
+            { id:"xyz",
+              name:"Girls Shirts",
+              price :63,
+              desc:"Era Embroided dress",
+              img:"/imgs/g2.png"},
+              { id:"znb",
+                name:"Girls Maxi",
+                price :65,
+                desc:"Era Embroided Maxi",
+                img:"/imgs/g3.png"},
+                { id:"ynb",
+                  name:"Girls Shirts",
+                  price :61,
+                  desc:"Black Women Kurta",
+                  img:"/imgs/g4.png"},         
+    {
+      id:"abc",
+      name:"Girls Accessories",
+      price :45,
+      desc:"Beautiful white pearls Bracelet",
+      img:"/imgs/a1.jpg"
+    },
+    {
+    id:"cba",
+    name:"Girls Accessories",
+    price :60,
+    desc:"Zirconia Bracelet Korean Style",
+    img:"/imgs/a2.png"
+  },
+  {
+    id:"caa",
+    name:"Girls Accessories",
+    price :25,
+    desc:"Crystal Bow Pendant",
+    img:"/imgs/a3.png"
+  },
+  {
+    id:"baa",
+    name:"Girls Accessories",
+    price :30,
+    desc:"Silver Rinestone Bracelet",
+    img:"/imgs/a4.png"
+  },
+    {
+      id:"def",
+      name:"Fragrance",
+      price :75,
+      desc:"Sweet Smell",
+      img:"/imgs/f1.jpg"
+    },
+    {
+      id:"dee",
+      name:"Fragrance",
+      price :95,
+      desc:"VC BodyMist Pure Seduction",
+      img:"/imgs/f2.png"
+    },
+    {
+      id:"eee",
+      name:"Fragrance",
+      price :85,
+      desc:"Glamour Body Mist For Women",
+      img:"/imgs/f3.png"
+    },
+    {
+      id:"efe",
+      name:"Fragrance",
+      price :88,
+      desc:"Christian Dior Joy",
+      img:"/imgs/f4.png"
+    },
+      {
+        id:"jkl",
+        name:"Mens Watch",
+        price :40,
+        desc:"Silver Chain Watch",
+        img:"/imgs/w2.png"
+      },
+      {
+        id:"lkj",
+        name:"Mens Watch",
+        price :44,
+        desc:"Black PU Strap Watch",
+        img:"/imgs/w3.png"
+      },
+      {
+        id:"lj",
+        name:"Womens Watch",
+        price :41,
+        desc:"Rhinestone Steel Belt Watch",
+        img:"/imgs/w4.png"
+      },
+      {
+        id:"oj",
+        name:"Womens Watch",
+        price :55,
+        desc:"Luxury Fashion Women Watch",
+        img:"/imgs/w5.png"
+      },
+  ];
